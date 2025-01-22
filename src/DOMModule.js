e9244316b6173e567e52e17115cbec26868fd3f9
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-01-21 21:50:04 -0800

    Implemented the DOM Module
    Added render and clear methods for the DOM

---
+++ src/DOMModule.js
@@ -0,0 +1,62 @@
+// import { player1, player2 } from "./index.js";
+import { Player } from "./objectFactories.js";
+export { renderBoard };
+
+// const p1 = player1;
+// const p2 = player2;
+
+// const player1 = new Player("player 1", false);
+// player1.board.createNewBoard();
+// const player2 = new Player("pc", true);
+// player2.board.createNewBoard();
+
+function clearBoard(player, whichBoard) {
+  if (document.querySelector(".div") != null) {
+    let boardWrap;
+    whichBoard == "p1"
+      ? (boardWrap = document.querySelector(".boardP1"))
+      : (boardWrap = document.querySelector(".boardP2"));
+    while (boardWrap.hasChildNodes()) {
+      boardWrap.removeChild(boardWrap.firstChild);
+    }
+    // for (let i = 0; i < player.board.board.length; i++) {
+    //   const div = document.querySelector(".div");
+    //   div.remove();
+    // }
+  }
+}
+
+function renderBoard(player, board) {
+  board.className == "boardP1"
+    ? clearBoard(player, "p1")
+    : clearBoard(player, "p2");
+  //   clearBoard(player);
+  for (let i = 0; i < player.board.board.length; i++) {
+    const div = document.createElement("div");
+    div.id = "r" + i;
+    div.classList.add("div");
+
+    for (let j = 0; j < player.board.board[i].length; j++) {
+      const div2 = document.createElement("div2");
+      div2.id = "r" + i + j;
+      div2.classList.add("div2");
+      if (player.board.board[i][j].empty == false) {
+        if (player.board.board[i][j].missedShot === true)
+          div2.textContent = "00";
+        if (player.board.board[i][j].ship === true) div2.textContent = "S";
+        if (
+          player.board.board[i][j].ship != null &&
+          player.board.board[i][j].isShot === true
+        )
+          div2.textContent = "XX";
+      }
+      div.appendChild(div2);
+    }
+    board.appendChild(div);
+  }
+}
+
+// renderBoard(player1, p1Board);
+
+// player1.board.receiveAttack(3, 4);
+// renderBoard(player1, p1Board);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 // import "./styles.css";
-import { Ship, Board } from "./objectFactories.js";
+import { Ship, Board, Player } from "./objectFactories.js";
+import { renderBoard } from "./DOMModule.js";
+// export { player1 , player2 };
 
 console.log("Index script Connected, line 3.");
 // console.log(new Ship());
 
-const testShip = new Ship(2);
-const testBoard = new Board();
-testBoard.createNewBoard();
-const testShip2 = new Ship(3);
+// const testShip = new Ship(2);
+// const testBoard = new Board();
+// testBoard.createNewBoard();
+// const testShip2 = new Ship(3);
 
 // testBoard.placeShip(3, 4, testShip2);
 // testBoard.receiveAttack(3, 4);
 // testBoard.anyRemainingShips();
+
+const p1Board = document.querySelector(".boardP1");
+const p2Board = document.querySelector(".boardP2");
+
+const player1 = new Player("player 1", false);
+player1.board.createNewBoard();
+const player2 = new Player("pc", true);
+player2.board.createNewBoard();
+
+renderBoard(player1, p1Board);
+renderBoard(player2, p2Board);
+
 console.log("test");
