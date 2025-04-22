bba5bb47981ba7737efb335694296218bf0da145
Ricardo <138608018+ric-projects@users.noreply.github.com>
2025-04-22 10:14:58 -0700

    Deployment commit

---
+++ 3bbdf9bfe4f09105c1e3.js
@@ -0,0 +1,49 @@
+import "./styles.css";
+import { Ship, Player, attack, placeShip } from "./objectFactories.js";
+import { renderBoard } from "./DOMModule.js";
+// export { attack };
+// export { player1 , player2 };
+
+console.log("Index script Connected, line 3.");
+// console.log(new Ship());
+
+// const testShip = new Ship(2);
+// const testBoard = new Board();
+// testBoard.createNewBoard();
+// const testShip2 = new Ship(3);
+
+// testBoard.placeShip(3, 4, testShip2);
+// testBoard.receiveAttack(3, 4);
+// testBoard.anyRemainingShips();
+
+// const p1Board = document.querySelector(".boardP1");
+// const p2Board = document.querySelector(".boardP2");
+
+// const player1 = new Player("player 1", false);
+// player1.board.createNewBoard();
+// renderBoard(player1, p1Board);
+// const player2 = new Player("pc", true);
+// player2.board.createNewBoard();
+// renderBoard(player2, p2Board);
+
+// function attack(x, y, player, DOMboard) {
+//   player.board.receiveAttack(x, y);
+//   renderBoard(player, DOMboard);
+// }
+
+// function placeShip(x, y, length, player, DOMboard) {
+//   if (y + length > 9) y = 10 - length;
+//   const newShip = new Ship(length);
+//   player.board.placeShip(x, y, newShip);
+//   renderBoard(player, DOMboard);
+// }
+
+// placeShip(3, 4, 2, player1, p1Board);
+// placeShip(2, 5, 3, player2, p2Board);
+// placeShip(7, 9, 5, player1, p1Board);
+// placeShip(1, 2, 4, player2, p2Board);
+
+// attack(2, 5, player2, p2Board);
+// attack(3, 3, player1, p1Board);
+
+console.log("test");
